56c1a891578d30b6d75e6e00a25a02f4b755a5b9
Tolik38 <trostinavusa@gmail.com>
2024-05-11 20:09:40 -0700

    20%

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,15 +1,9 @@
-//
-//  ViewController.swift
-//  Clima
-//
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
@@ -17,8 +11,39 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
-
+    // Exit button
+    @IBAction func Exit(_ sender: UIButton) {
+        exit(0)
+    }
+    
+    // Search button
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) // close keybutton
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true) // close keybutton
+        return true
+    }
+    
+    // if textField is empty "Type someting"
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type someting"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Use searchTextField.text to get the weather for that city.
+        
+        
+        searchTextField.text = "" //clear previos text
+    }
 
 }
 
